25b9a1370c72641ef32825a33a6452ad91fa825e
Volodymyr Kryvenko <Kryvenko.Volodymyr@gmail.com>
2016-10-07 13:22:25 +0300

    FielService created and successfully @Autowired in StubDataLoaderImpl need service methods implementation to continue

---
+++ legalist-service/src/main/java/ua/legalist/service/FieldServiceImpl.java
@@ -0,0 +1,10 @@
+package ua.legalist.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service("fieldService")
+public class FieldServiceImpl implements FieldService {
+
+}
--- legalist-service/src/main/java/ua/legalist/service/util/StubDataLoaderImpl.java
+++ legalist-service/src/main/java/ua/legalist/service/util/StubDataLoaderImpl.java
@@ -2,6 +2,7 @@ package ua.legalist.service.util;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ua.legalist.service.FieldService;
 import ua.legalist.service.NodeService;
 import ua.legalist.service.ProcessService;
 import ua.legalist.service.UserService;
@@ -17,6 +18,9 @@ public class StubDataLoaderImpl implements StubDataLoader {
     
     @Autowired
     ProcessService processService;
+    
+    @Autowired
+    FieldService fieldService;
     
     private boolean dataLoaded = false;
 

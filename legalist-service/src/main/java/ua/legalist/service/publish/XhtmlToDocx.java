5e3b7293c576631b1df334f055bffdf366435258
Volodymyr Kryvenko <Kryvenko.Volodymyr@gmail.com>
2016-11-14 13:07:00 +0200

    created XhtmlToDocx.java

---
+++ legalist-service/src/main/java/ua/legalist/service/publish/XhtmlToDocx.java
@@ -0,0 +1,9 @@
+
+package ua.legalist.service.publish;
+
+/**
+ * sample: https://github.com/plutext/docx4j-ImportXHTML/blob/master/src/samples/java/org/docx4j/samples/XhtmlToDocxAndBack.java
+ */
+public class XhtmlToDocx {
+    
+}
